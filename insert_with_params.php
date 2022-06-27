5f706dfac334c8fdf9953302c8085f670a00bc82
Ngo Minh Quan <ngominhquan20021809@gmail.com>
2022-06-28 06:25:35 +0700

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	insert_with_params.php

--- insert_with_params.php
+++ insert_with_params.php
@@ -16,23 +16,24 @@
     }
 
     // truyền dữ liệu vào API
-    if(isset($_GET['fullname'])&&isset($_GET['username'])&&isset($_GET['password']))
-    {
+//     if(isset($_GET['fullname'])&&isset($_GET['username'])&&isset($_GET['password']))
+//     {
         $FullName = $_GET['fullname']// nhận lien $_GET['truong1'] làm giá trị
         $Username = $_GET['username']
         $Password = $_GET['password']
         $namedatabase = "UserDemo1"
 
-        $sql = "INSERT INTO UserDemo1 VALUES(0,'$FullName', '$Username','$Password')";
+        $sql = "INSERT INTO UserDemo1 (id,fullname,username,password)
+        VALUES(0,'$FullName', '$Username','$Password')";
 
         if ($con->query($sql)==true){
             echo "ban ghi moi da dc them";
         }else{
             echo "Loi ".con->error;
         }
-    }else{
-        echo "Loi get method".con->error;
-    }
+//     }else{
+//         echo "Loi get method".con->error;
+//     }
 
     
     $con->close();
